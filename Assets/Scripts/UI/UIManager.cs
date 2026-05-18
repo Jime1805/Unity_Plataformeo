753bc56c799a54a2945b684352a170aa12a87124
Jime1805 <18ejimen@gmail.com>
2026-05-18 10:44:21 +0200

    pause acabat

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,12 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Game Over")]
     [SerializeField] private GameObject gameOverScreen;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
 
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
     }
+    #region Game Over
     public void GameOver()
     {
         gameOverScreen.SetActive(true);    
@@ -27,6 +33,38 @@ public class UIManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+        #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
+    #endregion 
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pauseScreen.activeInHierarchy)
+                PauseGame(false);
+            else
+                PauseGame(true);
+        }   
+    }
+
+    #region Pause
+    public void PauseGame(bool status)
+    {
+        pauseScreen.SetActive(status);
+        if(status)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+
+    }
+
+    public void PauseRestart()
+    {
+        Time.timeScale = 1; // Importante resetear antes de cargar
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    #endregion
 }
